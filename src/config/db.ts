cafe4c33aba58c72bc5cf9f6db5bd945553a5215
90cris <166570889+90cris@users.noreply.github.com>
2025-04-30 12:20:26 -0400

    config deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,24 +1,7 @@
+import "dotenv/config";
 import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
-
-export const db = new Pool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  allowExitOnIdle: true,
+export const DB = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } 
 });
-
-
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// const DB = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false } // obligatorio para Railway
-// });
-
-// module.exports = { DB };
